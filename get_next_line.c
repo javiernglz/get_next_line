5c4a4f9d9a144b59b5249372d78084ac75cfdfb1
Francisco Javier Navarrete Gonzalez <frnavarr@c3r8s4.42malaga.com>
2024-11-08 13:13:15 +0100

    ft_read_line on get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,30 +6,77 @@
 /*   By: frnavarr <frnavarr@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 13:19:10 by frnavarr          #+#    #+#             */
-/*   Updated: 2024/11/04 16:07:11 by frnavarr         ###   ########.fr       */
+/*   Updated: 2024/11/08 13:11:26 by frnavarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-	//comprobar fd y las asignaciones de memoria
-	//llamar ft para leer fd hasta encontrar \n o \0
-	//liberar buffer para evitar fugas de memoria
-	//fijar linea y retornar la linea con ft
+// lee desde el fd y llena el buffer
+// Lee caracteres BUFFER_SIZE en cada iteración hasta que 
+// haya un carácter \n o \0 en el búfer de línea.
+
+//bytes_read = 1 para poder entrar en el while y comenzar la lectura
+static char	*read_line(int fd, char *accumulated_buffer, char *read_buffer)
+{
+	ssize_t	bytes_read;
+	char	*tmp;
+
+	bytes_read = read(fd, read_buffer, BUFFER_SIZE);
+	bytes_read = 1;
+	while (bytes_read > 0)
+	{
+//		ssize_t	 	 read(int fd, void *buffer, size_t nbyte);
+//read lee hasta BUFFER_SIZE bytes de fd y los almacena en read_buffer
+//devuelve el numero de bytes leidos y lo asigna a bytes_read
+		if (bytes_read == -1)
+		{
+			free(accumulated_buffer);
+			return (NULL);
+		}
+		else if (bytes_read == 0)
+			break ;
+		read_buffer[bytes_read] = 0;
+		if (!accumulated_buffer)
+		{
+			accumulated_buffer = ft_strdup("");
+			tmp = accumulated_buffer;
+			accumulated_buffer = ft_strjoin(tmp, read_buffer);
+			free(tmp);
+			tmp = NULL;
+		}
+		if (ft_strchr(read_buffer, '\n'))
+			break ;
+	}
+	return (accumulated_buffer);
+}
+
+/* //extrae la linea del buffer hasta el caracter de nueva linea
+char	*extract_line(char *line_buffer)
+{
+	int	i;
+
+	while (line_buffer[i] && line_buffer[i] != '\n')
+		i++;
+} */
+
 char	*get_next_line(int fd)
 {
-	char	*buffer;
-	char	*line;
-	char	*tmp;
-	ssize_t	bytes_read;
-	
-	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	char		*buffer;
+	char		*line;
+	char		*tmp;
+	ssize_t		bytes_read;
+	static char	*accumulated_buffer;
+
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	line = ft_strdup("");
-	if (!line)
-		return (NULL);
+	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buffer)
+		return (NULL);
+	line = read_line(fd, accumulated_buffer, buffer);
+	free(buffer);
+	buffer = NULL;
+	if (!line)
 		return (NULL);
 	while ((bytes_read = read(fd, buffer, BUFFER_SIZE)) > 0)
 	{
@@ -47,16 +94,16 @@ char	*get_next_line(int fd)
 	if (bytes_read < 0 || (bytes_read == 0 && line[0] == '\0'))
 	{
 		free(line);
-		return (NULL);
+		return (ft_strdup(""));
 	}
 	return (line);
 }
 
-int main(void)
+/* int	main(void)
 {
 	int		fd;
 	char	*line;
-	
+
 	fd = open("libro.txt", O_RDONLY);
 	if (fd == -1)
 	{
@@ -71,3 +118,4 @@ int main(void)
 	close(fd);
 	return (0);
 }
+ */
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: frnavarr <frnavarr@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 16:03:39 by frnavarr          #+#    #+#             */
-/*   Updated: 2024/11/04 16:01:51 by frnavarr         ###   ########.fr       */
+/*   Updated: 2024/11/08 12:37:31 by frnavarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,50 +42,12 @@ char	*ft_strdup(const char *s)
 	return (copy);
 }
 
-// lee desde el fd y llena el buffer
-// Lee caracteres BUFFER_SIZE en cada iteración hasta que 
-// haya un carácter \n o \0 en el búfer de línea.
-
-//bytes_read = 1 para poder entrar en el while y comenzar la lectura
-/* static char	*read_line(int fd, char *read_buffer, char *accumulated_buffer)
-{
-	ssize_t	bytes_read;
-	
-	bytes_read = 1;
-	while (bytes_read > 0)
-	{
-//		ssize_t	 	 read(int fildes, void *buffer, size_t nbyte);
-//read lee hasta BUFFER_SIZE bytes de fd y los almacena en read_buffer
-//devuelve el numero de bytes leidos y lo asigna a bytes_read
-		bytes_read = read(fd, read_buffer, BUFFER_SIZE);
-		if (bytes_read == -1)
-			return (NULL);
-		else if (bytes_read == 0)
-			break;
-	}
-} */
-
-//extrae la linea del buffer hasta el caracter de nueva linea
-/* char	*extract_line(char *line_buffer)
-{
-	int	i;
-	
-	while (line_buffer[i] && line_buffer[i] != '\n')
-		i++;
-} */
-
-//borrar el contenido restante del buffer dsp de extraer
-//clean_buffer
-
-//libera la memoria asignada para evitar fugas
-//mem_free 
-
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*join;
 	size_t	i;
 	size_t	j;
-	
+
 	if (!s1 && !s2)
 		return (NULL);
 	if (!s1)
